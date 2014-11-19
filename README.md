df705595b393aa6c441e9beb6797d15a170b7dd1
umangmathur92 <umangmathur10051992@gmail.com>
2014-11-19 10:06:47 +0530

    added ability ro search for packages

--- README.md
+++ README.md
@@ -2,22 +2,3 @@ android-data-folder-accessor
 ============================
 
 A simple Bash Script to access the contents of /data/data folder of any android application.
-
-
-Instructions :
-
-1)Open Terminal
-
-2)Type 'sudo su' to get root privileges
-
-3)Navigate to folder where you have saved androidDataAccessor.sh within the terminal
-
-4)Type 'chmod +x androidDataAccessor.sh'
-
-5)Type './androidDataAccessor.sh'
-
-6)Follow the On-screen instructions
-
-
-
-Note: Tested only on Ubuntu. Prerequisite : Python's Zlib library. 
--- androidDataAccessor.sh
+++ androidDataAccessor.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+echo 'Enter the name of your application'
+read appname
+echo '==========================================================='
+./adb shell pm list packages -e $appname
+echo '==========================================================='
+echo 'A list of package names matching your application name are listed above. Determine which one belongs to your application.'
 echo 'Enter the package name of your application'
 read packagename
 echo 'You entered package name as : '$packagename
 ./adb backup -noapk $packagename
 java -jar abe.jar unpack backup.ab $packagename.tar
 rm backup.ab
+echo 'All the application data has been extracted to a .tar archive. Please ensure that you have appropriate software instaled on your machine to extract the same'
