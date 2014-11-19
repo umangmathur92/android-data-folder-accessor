6dd928ace675eb5da76c7a8c93441fc00d98f212
umangmathur92 <umangmathur10051992@gmail.com>
2014-11-19 09:36:50 +0530

    used android backup extractor jar instead of openssl for extracting .ab to tar

--- androidDataAccessor.sh
+++ androidDataAccessor.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
-echo 'Ensure that you have Python Zlib Library installed before continuing'
-echo 'Enter the absolute path to adb Directory(platform-tools folder)'
-read path
 echo 'Enter the package name of your application'
 read packagename
-echo 'Enter the absolute destination path'
-read destination
-echo 'You entered path as : '$path
 echo 'You entered package name as : '$packagename
-echo 'The .tar archive file will be saved at : '$destination
-cd $path
 ./adb backup -noapk $packagename
-cd $path
-cat backup.ab | (dd bs=24 count=0 skip=1; cat) | zlib-flate -uncompress > $packagename.tar
+java -jar abe.jar unpack backup.ab $packagename.tar
 rm backup.ab
-cp $packagename.tar $destination
-rm $packagename.tar
